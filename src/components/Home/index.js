e29dc956632f4a37226a618e5561c2eaa5bebb5e
vinay <vinaypallerla5664@gmail.com>
2023-05-12 20:23:15 +0000

    fetch and routing practice

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import BlogList from '../BlogList'
+import UserInfo from '../UserInfo'
+import './index.css'
+
+const Home = () => (
+  <div>
+    <UserInfo />
+    <BlogList />
+  </div>
+)
+export default Home
